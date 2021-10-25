bf1bb35df94ff6324843b1c9504d39c6b515657d
Rounak Meyur <45717264+rounak-meyur@users.noreply.github.com>
2021-10-25 16:11:20 -0400

    updated codes

--- opt-usage-toyexamp.py
+++ opt-usage-toyexamp.py
@@ -10,6 +10,7 @@ import numpy as np
 import networkx as nx
 import pandas as pd
 from math import ceil
+from shapely.geometry import LineString
 
 workpath = os.getcwd()
 rootpath = os.path.dirname(workpath)
@@ -25,27 +26,6 @@ homepath = inppath + "VA121_20140723.csv"
 sys.path.append(libpath)
 from pyLoadlib import Load
 print("Imported modules")
-
-
-#%% Network definition
-dist = nx.Graph()
-edgelist = [(0,11),(11,12),(12,13),(13,14),(11,1),(1,2),(12,3),(12,4),
-            (14,5),(5,6)]
-dist.add_edges_from(edgelist)
-nlabel = {0:'S',1:'H',2:'H',3:'H',4:'H',5:'H',6:'H',
-          11:'T',12:'T',13:'R',14:'T'}
-ncord = {0:[0,0],1:[1,-1],2:[1,-2],3:[2,-1],4:[2,1],5:[4,-1],6:[4,-2],
-         11:[1,0],12:[2,0],13:[3,0],14:[4,0]}
-elabel = {(0,11):'P',(11,12):'P',(12,13):'P',(13,14):'P',
-          (11,1):'S',(1,2):'S',(12,3):'S',(12,4):'S',(14,5):'S',(5,6):'S'}
-e_r = {(0,11):0.001,(11,12):0.001,(12,13):0.001,(13,14):0.001,
-          (11,1):0.0005,(1,2):0.0005,(12,3):0.0005,(12,4):0.0005,(14,5):0.0005,(5,6):0.0005}
-
-nx.set_edge_attributes(dist, e_r, 'r')
-nx.set_edge_attributes(dist, elabel, 'label')
-
-nx.set_node_attributes(dist, ncord, 'cord')
-nx.set_node_attributes(dist, nlabel, 'label')
 
 
 
@@ -100,21 +80,9 @@ homes = get_home_data(homepath)
 # Store the home data for the following houses with IDs from the toy network
 homeid = [511210207001189,51121020900342,511210203001855,511210207001113,
           51121020900346,51121021300494]
+homeid = [511210207001189]*6
 homes = {k+1:homes[h] for k,h in enumerate(homeid)}
 
-
-
-#%% Main code
-for n in dist:
-    dist.nodes[n]['load'] = [0.0]*24
-
-# Cost profile taken for the off peak plan of AEP
-COST = [0.073626]*5 + [0.092313]*10 + [0.225525]*3 + [0.092313]*6
-# COST = [0.093626]*5 + [0.072313]*10 + [0.225525]*3 + [0.092313]*6
-
-
-# Feed in tarriff rate for small residence owned rooftop solar installations
-FEED = 0.38
 
 #%% Optimize power usage schedule
 
@@ -158,7 +126,7 @@ def powerflow(graph, iterdata,
     edge_r = []
     for e in graph.edges:
         try:
-            edge_r.append(1.0/graph.edges[e]['r'])
+            edge_r.append(1.0/(graph.edges[e]['r']))
         except:
             edge_r.append(1.0/1e-14)
     R = np.diag(edge_r)
@@ -184,15 +152,53 @@ def powerflow(graph, iterdata,
     iterdata["volt"] = {n:V[i,:] for i,n in enumerate(nodelist)}
     return iterdata
     
+#%% Cost Profile
 
+# Cost profile taken for the off peak plan of AEP
+# COST = [0.073626]*5 + [0.092313]*10 + [0.225525]*3 + [0.092313]*6
+# COST = [0.093626]*5 + [0.072313]*10 + [0.225525]*3 + [0.092313]*6
+COST = [1]*24
+
+# Feed in tarriff rate for small residence owned rooftop solar installations
+FEED = 0.38
+
+#%% Network definition
+dist = nx.Graph()
+edgelist = [(0,10),(10,11),(11,12),(12,13),(13,14),(11,1),(1,2),(12,3),(12,4),
+            (14,5),(5,6)]
+dist.add_edges_from(edgelist)
+nlabel = {0:'S',1:'H',2:'H',3:'H',4:'H',5:'H',6:'H',
+          10:'R',11:'T',12:'T',13:'R',14:'T'}
+ncord = {0:[0,0],1:[1,-1],2:[1,-2],3:[2,-1],4:[2,1],5:[4,-1],6:[4,-2],
+         10:[0.5,0],11:[1,0],12:[2,0],13:[3,0],14:[4,0]}
+elabel = {(0,10):'E',(10,11):'P',(11,12):'P',(12,13):'P',(13,14):'P',
+          (11,1):'S',(1,2):'S',(12,3):'S',(12,4):'S',(14,5):'S',(5,6):'S'}
+
+util = 50
+prefix = "samerate-"+"net4"
+
+e_r = {(0,10):1e-12, (10,11):0.001*util,(11,12):0.001*util,(12,13):0.001*util,(13,14):0.001*util,
+          (11,1):0.0005,(1,2):0.0005,(12,3):0.0005,(12,4):0.0005,(14,5):0.0005,(5,6):0.0005}
+
+nx.set_edge_attributes(dist, e_r, 'r')
+nx.set_edge_attributes(dist, elabel, 'label')
+
+nx.set_node_attributes(dist, ncord, 'cord')
+nx.set_node_attributes(dist, nlabel, 'label')
+
+for e in edgelist:
+    dist.edges[e]['geometry'] = LineString((ncord[e[0]],ncord[e[1]]))
+
+for n in dist:
+    dist.nodes[n]['load'] = [0.0]*24
 
 
 #%% Iterative algorithm
 
 nodelist = [node for node in list(dist.nodes()) if dist.nodes[node]['label'] != 'S']
 ITERDATA = {"alpha":{n:[0.0]*24 for n in nodelist},
-            "mu_low":{n:[10.0]*24 for n in nodelist},
-            "mu_up":{n:[9.0]*24 for n in nodelist},
+            "mu_low":{n:[1.0]*24 for n in nodelist},
+            "mu_up":{n:[1.0]*24 for n in nodelist},
             "volt": {n:[1.0]*24 for n in nodelist},
             "load":{n:[0.0]*24 for n in nodelist},
             "sl":{n:[0.0]*24 for n in nodelist}}
@@ -202,10 +208,10 @@ alpha_history = {}
 volt_history = {}
 load_history = {}
 sl_history = {}
-iterations = 1
+iterations = 4
 
 k = 0
-while(k <= 1):
+while(k <= iterations):
     ITERDATA["load"],ITERDATA["sl"] = compute_power_schedule(dist,homes,
                                               COST,FEED,ITERDATA["alpha"])
     # update 
@@ -217,7 +223,7 @@ while(k <= 1):
     sl_history[k] = ITERDATA["sl"]
     k = k + 1
 
-#%% Plot incentive evolution
+#Plot incentive evolution
 import matplotlib.pyplot as plt
 
 homelist = [n for n in dist if dist.nodes[n]['label'] == 'H']
@@ -232,44 +238,40 @@ xarray = np.linspace(0,25,25)
 #         ax1.step(xarray,volt_history[m][h],label="home="+str(h))
 #         ax1.legend(ncol=3)
 
-
-# fig2 = plt.figure(figsize=(20,16))
-# ax2 = fig2.add_subplot(6,1,1)
-# for i,h in enumerate(homelist):
-#     ax2.step(xarray,homes[h]["ORG"]["total"],label="home"+str(h))
-#     ax2.legend(ncol=3)
-# for m in range(5):
-#     ax2 = fig2.add_subplot(6,1,m+2)
-#     for i,h in enumerate(homelist):
-#         ax2.step(xarray,load_history[m][h],label="home="+str(h))
-#         ax2.legend(ncol=3)
-
-
-# fig3 = plt.figure(figsize=(20,16))
-# for m in range(5):
-#     ax3 = fig3.add_subplot(5,1,m+1)
-#     for i,h in enumerate(homelist):
-#         ax3.step(xarray,alpha_history[m][h],label="home="+str(h))
-#         ax3.legend(ncol=3)
-
-
 for device in ['laundry','dwasher']:
     fig = plt.figure(figsize = (20,16))
     for i,h in enumerate(homelist):
         ax = fig.add_subplot(6,1,i+1)
         if device in homes[h]["SL"]:
-            ax.step(xarray,[0]+homes[h]["ORG"][device],label="original profile")
-            for m in range(2):
+            # ax.step(xarray,[0]+homes[h]["ORG"][device],label="original profile")
+            for m in range(iterations):
                 ax.step(xarray,[0]+sl_history[m][h][device],
-                        label="home="+str(h)+", iter="+str(m+1))
-            ax.legend(ncol=3)
+                        label="iter="+str(m+1))
+            ax.legend(ncol=5,prop={'size': 15})
+    fig.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-'+device),
+                bbox_inches='tight')
+
+fig2 = plt.figure(figsize=(20,16))
+for i,h in enumerate(homelist):
+    ax2 = fig2.add_subplot(6,1,i+1)
+    # ax2.step(xarray,[0]+homes[h]["ORG"]["total"],label="original profile")
+    for m in range(iterations):
+        ax2.step(xarray,[0]+load_history[m][h],
+                 label="iter="+str(m+1))
+        ax2.legend(ncol=5,prop={'size': 15})
+fig2.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-total'),
+             bbox_inches='tight')
 
 
-
-
-
-
-
+fig3 = plt.figure(figsize=(20,16))
+for i,h in enumerate(homelist):
+    ax3 = fig3.add_subplot(6,1,i+1)
+    for m in range(iterations):
+        ax3.step(xarray,np.insert(alpha_history[m][h],0,0),
+                 label="iter="+str(m+1))
+        ax3.legend(ncol=5,prop={'size': 15})
+fig3.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-incentive'),
+             bbox_inches='tight')
 
 
 
