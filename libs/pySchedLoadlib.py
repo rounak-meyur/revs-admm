b6b897fb5b178e388c48066a602d470ad96ab4f7
Rounak Meyur <45717264+rounak-meyur@users.noreply.github.com>
2021-12-01 14:27:00 -0500

    updated centralized alg

---
+++ libs/pySchedLoadlib.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 10:16:57 2021
+
+Author: Swapna Thorve and Rounak Meyur
+
+Description: Residence Load Device models
+"""
+
+import sys
+import gurobipy as grb
+import networkx as nx
+import numpy as np
+
+def compute_Rmat(graph):
+    A = nx.incidence_matrix(graph,nodelist=list(graph.nodes()),
+                            edgelist=list(graph.edges()),oriented=True).toarray()
+    node_ind = [i for i,node in enumerate(graph.nodes()) \
+                if graph.nodes[node]['label'] != 'S']
+    
+    # Resistance data
+    F = np.linalg.inv(A[node_ind,:].T)
+    D = np.diag([graph.edges[e]['r'] for e in graph.edges])
+    return 2*F@D@(F.T)
+
+#%% Function for callback
+def mycallback(model, where):
+    if where == grb.GRB.Callback.MIP:
+        # General MIP callback
+        objbst = model.cbGet(grb.GRB.Callback.MIP_OBJBST)
+        objbnd = model.cbGet(grb.GRB.Callback.MIP_OBJBND)
+        time = model.cbGet(grb.GRB.Callback.RUNTIME)
+        if (time>300 and abs(objbst - objbnd) < 0.05 * (1.0 + abs(objbst))):
+            print('Stop early - 5% gap achieved')
+            model.terminate()
+        elif(time>60 and abs(objbst - objbnd) < 0.01 * (1.0 + abs(objbst))):
+            print('Stop early - 1% gap achieved')
+            model.terminate()
+    return
+
+#%% Functions for optimization problem
+
+class Central:
+    def __init__(self,homedata,graph,cost):
+        self.model = grb.Model(name="Get Optimal Schedule")
+        self.model.ModelSense = grb.GRB.MINIMIZE
+        self.p = {}
+        self.g = {}
+        self.a = {}
+        self.data = homedata
+        self.w = len(cost)
+        
+        # Add all load devices
+        for h in homedata:
+            self.add_SL(h)
+            self.add_fixed(h)
+        
+        # Add network constraints and incentive
+        self.network(graph,cost)
+        self.set_objective(graph, cost)
+        
+        return
+    
+    def add_SL(self,h):
+        u = {}
+        SL_data = self.data[h]["SL"]
+        for d in SL_data:
+            # Schedulable Load parameters
+            prate = SL_data[d]["rating"]
+            tschd = SL_data[d]["time"]
+            for t in range(self.w):
+                # Variables
+                u[(h,d,t)] = self.model.addVar(vtype=grb.GRB.BINARY,
+                                name="u_{0}_{1}_{2}".format(h,d,t))
+                self.p[(h,d,t)] = self.model.addVar(vtype=grb.GRB.CONTINUOUS,
+                                name="p_{0}_{1}_{2}".format(h,d,t))
+                
+            # Constraints
+            self.model.addConstr(
+                grb.quicksum([u[(h,d,t)] for t in range(self.w)]) == tschd)
+            for t in range(self.w):
+                self.model.addConstr(self.p[(h,d,t)] == prate*u[(h,d,t)])
+            for k in range(self.w-tschd):
+                if k==0:
+                    self.model.addConstr(
+                        grb.quicksum([u[(h,d,t)] \
+                                      for t in range(k,k+tschd)]) >= \
+                            tschd*u[(h,d,k)])
+                else:
+                    self.model.addConstr(
+                        grb.quicksum([u[(h,d,t)] \
+                                      for t in range(k,k+tschd)]) >= \
+                            tschd*(u[(h,d,k)]-u[(h,d,k-1)]))
+        return
+    
+    def add_fixed(self,h):
+        fixed_data = self.data[h]["FIXED"]
+        for d in fixed_data:
+            for t in range(self.w):
+                self.p[(h,d,t)] = fixed_data[d][t]
+        return
+    
+    def network(self,graph,c0,vmin=0.95,vmax=1.05):
+        R = compute_Rmat(graph)
+        vlow = (vmin*vmin - 1)
+        vhigh = (vmax*vmax - 1)
+        nodelist = [n for n in graph.nodes() if graph.nodes[n]['label'] != 'S']
+        for n in nodelist:
+            for t in range(self.w):
+                self.g[(n,t)] = self.model.addVar(vtype=grb.GRB.CONTINUOUS,
+                                            name="g_{0}_{1}".format(n,t))
+                if graph.nodes[n]['label']=='H':
+                    devices = [d for dtype in self.data[n] if dtype != "ORG" \
+                                for d in self.data[n][dtype]]
+                    self.model.addConstr(
+                        self.g[(n,t)] == grb.quicksum([self.p[(n,d,t)] \
+                                                        for d in devices]))
+                else:
+                    self.model.addConstr(self.g[(n,t)] == 0)
+        
+        # Add voltage constraints
+        for i,_ in enumerate(nodelist):
+            for t in range(self.w):
+                self.model.addConstr(
+                    -grb.quicksum([R[i,j]*self.g[(m,t)] \
+                                  for j,m in enumerate(nodelist)]) <= vhigh)
+                # self.model.addConstr(
+                #     -grb.quicksum([R[i,j]*self.g[(m,t)] \
+                #                   for j,m in enumerate(nodelist)]) >= vlow)
+        
+        for n in nodelist:
+            for t in range(self.w):
+                self.a[(n,t)] = self.model.addVar(vtype=grb.GRB.CONTINUOUS,
+                                    name="a_{0}_{1}".format(n,t))
+                self.model.addConstr(self.a[(n,t)] >= 0)
+                self.model.addConstr(self.a[(n,t)] <= c0[t]*self.g[(n,t)])
+        return
+    
+    def set_objective(self,graph,c0):
+        nodelist = [n for n in graph.nodes() if graph.nodes[n]['label'] != 'S']
+        obj = grb.quicksum([(c0[t]*self.g[(n,t)]) - self.a[(n,t)] \
+                            for n in nodelist for t in range(self.w)])
+        self.model.setObjective(obj)
+        return
+    
+    def solve(self,grbpath):
+        # Write the LP problem
+        self.model.write(grbpath+"load-schedule-central.lp")
+        
+        # Set up solver settings
+        grb.setParam('OutputFlag', 0)
+        grb.setParam('Heuristics', 0)
+        
+        # Open log file
+        logfile = open(grbpath+'gurobi-central.log', 'w')
+        
+        # Pass data into my callback function
+        self.model._lastiter = -grb.GRB.INFINITY
+        self.model._lastnode = -grb.GRB.INFINITY
+        self.model._logfile = logfile
+        self.model._vars = self.model.getVars()
+        
+        # Solve model and capture solution information
+        self.model.optimize(mycallback)
+        
+        # Close log file
+        logfile.close()
+        if self.model.SolCount == 0:
+            print('No solution found, optimization status = %d' % self.model.Status)
+            sys.exit(0)
+        else:
+            # Return the device schedules
+            fixed = ['base', 'hvac', 'hoth2o']
+            self.p_sch = {}
+            self.g_opt = {}
+            self.a_opt = {}
+            for h in self.data:
+                devices = [d for dtype in self.data[h] if dtype != "ORG" \
+                            for d in self.data[h][dtype] if d not in fixed]
+                p_opt = {(h,d,t): self.p[(h,d,t)].getAttr("x") for d in devices \
+                          for t in range(self.w)}
+            
+                # Store optimal schedule in the attribute
+                self.p_sch[h] = {d: [p_opt[(h,d,t)] for t in range(self.w)] for d in devices}
+                self.g_opt[h] = [self.g[(h,t)].getAttr("x") for t in range(self.w)]
+                self.a_opt[h] = [self.a[(h,t)].getAttr("x") for t in range(self.w)]
+            return
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- opt-usage-toy3.py
+++ opt-usage-toy3.py
@@ -24,7 +24,7 @@ homepath = inppath + "VA121_20140723.csv"
 
 
 sys.path.append(libpath)
-from pyLoadlib import Load
+# from pySchedLoadlib import Central
 print("Imported modules")
 
 
@@ -82,92 +82,9 @@ homeid = [511210207001189,51121020900342,511210203001855,511210207001113,
           51121020900346,51121021300494]
 
 
-
-#%% Optimize power usage schedule
-def compute_Rmat(graph):
-    A = nx.incidence_matrix(graph,nodelist=list(graph.nodes()),
-                            edgelist=list(graph.edges()),oriented=True).toarray()
-    node_ind = [i for i,node in enumerate(graph.nodes()) \
-                if graph.nodes[node]['label'] != 'S']
-    
-    # Resistance data
-    F = np.linalg.inv(A[node_ind,:].T)
-    D = np.diag([graph.edges[e]['r'] for e in graph.edges])
-    return F@D@(F.T)
-
-def compute_power_schedule(net,homes,cost,feed):
-    homelist = [n for n in net if net.nodes[n]['label'] == 'H']
-    incentive = nx.get_node_attributes(net,"alpha")
-    
-    # UPDATE load model for residences
-    for n in net:
-        if n in homelist:
-            # Compute load schedule
-            L = Load(24,homes[n])
-            L.set_objective(cost, feed, incentive[n])
-            L.solve(n,grbpath)
-        
-            # Update load data in network
-            net.nodes[n]["load"] = [v for v in L.g_opt]
-            net.nodes[n]["schedule"] = {d:L.p_sch[d] for d in L.p_sch}
-        else:
-            net.nodes[n]["load"] = [0.0]*24
-            net.nodes[n]["schedule"] = {}
-    return
-
-def powerflow(net,R):
-    """
-    Checks power flow solution and save dictionary of voltages.
-    """
-    nodelist = [n for n in net if net.nodes[n]['label'] != 'S']
-    # Power consumption and node voltage over time window
-    P = np.array([net.nodes[n]['load'] for n in nodelist])
-    V = 1.0 - np.matmul(R,P)
-    # Update the voltage data
-    for i,n in enumerate(nodelist):
-        net.nodes[n]["volt"] = V[i,:]
-    return
-
-
-def get_incentive_dual(net,R,eps=0.01,phi=1e-4,vmin=0.95,vmax=1.05):
-    # Get dual variables
-    nodelist = [n for n in net if net.nodes[n]['label'] != 'S']
-    mu_low = np.array([net.nodes[n]["mu_low"] for n in nodelist])
-    mu_up = np.array([net.nodes[n]["mu_up"] for n in nodelist])
-    V = np.array([net.nodes[n]["volt"] for n in nodelist])
-    
-    # Update dual
-    mu_low = (1-eps*phi)*mu_low + eps*(vmin - V)
-    mu_up = (1-eps*phi)*mu_up + eps*(V - vmax)
-    alpha = np.matmul(R,(mu_low-mu_up))
-    for i,n in enumerate(nodelist):
-        net.nodes[n]["alpha"] = alpha[i,:]
-        net.nodes[n]["mu_low"] = mu_low[i,:]
-        net.nodes[n]["mu_up"] = mu_up[i,:]
-    return
-
-
-def get_incentive(net):
-    # Get voltages
-    nodelist = [n for n in net if net.nodes[n]['label'] != 'S']
-    V = np.array([net.nodes[n]["volt"] for n in nodelist])
-    # A = np.array([net.nodes[n]["alpha"] for n in nodelist])
-    
-    # Update dual
-    alpha = (V - 0.98)/(1.0-np.amin(V))
-    for i,n in enumerate(nodelist):
-        net.nodes[n]["alpha"] = alpha[i,:]
-    return
-    
-#%% Cost Profile
-
-# Cost profile taken for the off peak plan of AEP
-# COST = [0.073626]*5 + [0.092313]*10 + [0.225525]*3 + [0.092313]*6
-# COST = [0.093626]*5 + [0.072313]*10 + [0.225525]*3 + [0.092313]*6
 COST = [1]*24
 
-# Feed in tarriff rate for small residence owned rooftop solar installations
-FEED = 0.38
+
 
 #%% Network definition
 dist = nx.Graph()
@@ -181,8 +98,8 @@ ncord = {0:[0,0],1:[1,-1],2:[1,-2],3:[2,-1],4:[2,1],5:[4,-1],6:[4,-2],
 elabel = {(0,10):'E',(10,11):'P',(11,12):'P',(12,13):'P',(13,14):'P',
           (11,1):'S',(1,2):'S',(12,3):'S',(12,4):'S',(14,5):'S',(5,6):'S'}
 
-util = 20
-prefix = "4low2highload"+"net20"+"pos0"
+util = 1
+prefix = "central-control"+"net0"+"pos0"
 # prefix = "alllowload"+"net0"+"pos0"
 
 e_r = {(0,10):1e-12, (10,11):0.001,(11,12):0.001,(12,13):0.001,(13,14):0.001*util,
@@ -200,45 +117,18 @@ nx.set_node_attributes(dist, nlabel, 'label')
 for e in edgelist:
     dist.edges[e]['geometry'] = LineString((ncord[e[0]],ncord[e[1]]))
 
-for n in dist:
-    dist.nodes[n]['load'] = [0.0]*24
-    dist.nodes[n]['alpha'] = [0.0]*24
-    dist.nodes[n]["mu_low"] = [1.0]*24
-    dist.nodes[n]["mu_up"] = [1.0]*24
-    
-R = compute_Rmat(dist)
+
 
 # Home data
 homeid = [51121021300494]*4+[511210207001189]*2
 # homeid = [511210207001189]*6
 # homeid = [51121021300494]*6
 homes = {k+1:all_homes[h] for k,h in enumerate(homeid)}
-sys.exit(0)
-#%% Iterative algorithm
 
-alpha_history = {}
-volt_history = {}
-load_history = {}
-sl_history = {}
-iterations = 5
-
-k = 0
-while(k <= iterations):
-    # update schedule
-    compute_power_schedule(dist,homes,COST,FEED)
-    
-    # update powerflow
-    powerflow(dist,R)
-    
-    # update incentive
-    # get_incentive_dual(dist,R)
-    get_incentive(dist)
-    
-    alpha_history[k] = nx.get_node_attributes(dist,"alpha")
-    volt_history[k] = nx.get_node_attributes(dist,"volt")
-    load_history[k] = nx.get_node_attributes(dist,"load")
-    sl_history[k] = nx.get_node_attributes(dist,"schedule")
-    k = k + 1
+#%% Central algorithm
+from pySchedLoadlib import Central
+c = Central(homes,dist,COST)
+c.solve(grbpath)
 
 #Plot incentive evolution
 import matplotlib.pyplot as plt
@@ -247,28 +137,23 @@ homelist = sorted([n for n in dist if dist.nodes[n]['label'] == 'H'])
 xarray = np.linspace(0,25,25)
 
 
+
 for device in ['laundry','dwasher']:
     fig = plt.figure(figsize = (20,16))
     for i,h in enumerate(homelist):
         ax = fig.add_subplot(6,1,i+1)
         if device in homes[h]["SL"]:
-            # ax.step(xarray,[0]+homes[h]["ORG"][device],label="original profile")
-            for m in range(iterations):
-                ax.step(xarray,[0]+sl_history[m][h][device],
-                        label="iter="+str(m+1))
-            ax.legend(ncol=5,prop={'size': 15})
+            ax.step(xarray,[0]+c.p_sch[h][device])
         ax.set_ylabel("Sch. Load (kW)",fontsize=15)
     fig.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-'+device),
                 bbox_inches='tight')
 
+
 fig2 = plt.figure(figsize=(20,16))
 for i,h in enumerate(homelist):
     ax2 = fig2.add_subplot(6,1,i+1)
-    # ax2.step(xarray,[0]+homes[h]["ORG"]["total"],label="original profile")
-    for m in range(iterations):
-        ax2.step(xarray,[0]+load_history[m][h],
-                 label="iter="+str(m+1))
-        ax2.legend(ncol=5,prop={'size': 15})
+    ax2.step(xarray,[0]+c.g_opt[h])
+        
     ax2.set_ylabel("Total load (kW)",fontsize=15)
 fig2.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-total'),
              bbox_inches='tight')
@@ -277,24 +162,12 @@ fig2.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-total'),
 fig3 = plt.figure(figsize=(20,16))
 for i,h in enumerate(homelist):
     ax3 = fig3.add_subplot(6,1,i+1)
-    for m in range(iterations):
-        ax3.step(xarray,np.insert(alpha_history[m][h],0,0),
-                 label="iter="+str(m+1))
-        ax3.legend(ncol=5,prop={'size': 15})
+    ax3.step(xarray,np.insert(c.a_opt[h],0,0))
     ax3.set_ylabel("Incentive signal",fontsize=15)
 fig3.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-incentive'),
              bbox_inches='tight')
 
-fig4 = plt.figure(figsize=(20,16))
-for i,h in enumerate(homelist):
-    ax4 = fig4.add_subplot(6,1,i+1)
-    for m in range(iterations):
-        ax4.step(xarray,np.insert(volt_history[m][h],0,1),
-                 label="iter="+str(m+1))
-        ax4.legend(ncol=5,prop={'size': 15})
-    ax4.set_ylabel("Voltage (pu)",fontsize=15)
-fig4.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-voltage'),
-             bbox_inches='tight')
+
 
 
 
