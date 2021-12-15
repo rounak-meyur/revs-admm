d43d59aeef661d9234a49ff9e418cb41c6e170f1
Rounak Meyur <45717264+rounak-meyur@users.noreply.github.com>
2021-12-15 18:10:50 -0500

    updated code

--- libs/pySchedLoadlib.py
+++ libs/pySchedLoadlib.py
@@ -277,9 +277,219 @@ class Simple:
                               for t in range(self.T)] for h in self.res}
             return
 
+#%% Distributed Solution Approach
 
+class Home:
+    def __init__(self,cost,homedata,p_est,p_sch,gamma):
+        self.c = cost
+        self.T = len(cost)
+        self.data = homedata
+        
+        self.model = grb.Model(name="Get Optimal Schedule")
+        self.model.ModelSense = grb.GRB.MINIMIZE
+        self.add_SL()
+        self.add_fixed()
+        self.netload_var()
+        self.set_objective(p_est,p_sch,gamma)
+        return
+    
+    def variables(self):
+        self.g = {t:self.model.addVar(vtype=grb.GRB.BINARY,name="g_{0}".format(t)) \
+                  for t in range(self.T)}
+        
+        # Add constraints for the variables
+        self.model.addConstr(grb.quicksum([self.g[t] for t in range(self.T)]) == 1)
+        return
+    
+    def netload_var(self):
+        self.g = {t:self.model.addVar(vtype=grb.GRB.CONTINUOUS,
+                                      name="g_{0}".format(t)) \
+                  for t in range(self.T)}
+        
+        # Add constraints for the variables
+        devices = [d for dtype in self.data if dtype != "ORG" \
+                    for d in self.data[dtype]]
+        for t in range(self.T):
+            self.model.addConstr(
+                self.g[t] == grb.quicksum([self.p[(d,t)] for d in devices]))
+        return
+    
+    def add_SL(self):
+        u = {}
+        self.p = {}
+        SL_data = self.data["SL"]
+        for d in SL_data:
+            # Schedulable Load parameters
+            prate = SL_data[d]["rating"]
+            tschd = SL_data[d]["time"]
+            for t in range(self.T):
+                # Variables
+                u[(d,t)] = self.model.addVar(vtype=grb.GRB.BINARY,
+                                name="u_{0}_{1}".format(d,t))
+                self.p[(d,t)] = self.model.addVar(vtype=grb.GRB.CONTINUOUS,
+                                name="p_{0}_{1}".format(d,t))
+                
+            # Constraints
+            self.model.addConstr(
+                grb.quicksum([u[(d,t)] for t in range(self.T)]) == tschd)
+            for t in range(self.T):
+                self.model.addConstr(self.p[(d,t)] == prate*u[(d,t)])
+            for k in range(self.T-tschd):
+                if k==0:
+                    self.model.addConstr(
+                        grb.quicksum([u[(d,t)] \
+                                      for t in range(k,k+tschd)]) >= \
+                            tschd*u[(d,k)])
+                else:
+                    self.model.addConstr(
+                        grb.quicksum([u[(d,t)] \
+                                      for t in range(k,k+tschd)]) >= \
+                            tschd*(u[(d,k)]-u[(d,k-1)]))
+        return
+    
+    def add_fixed(self):
+        fixed_data = self.data["FIXED"]
+        for d in fixed_data:
+            for t in range(self.T):
+                self.p[(d,t)] = fixed_data[d][t]
+        return
+    
+    def set_objective(self,p_util,p_res,gamma):
+        # main objective
+        obj1 = grb.quicksum([(self.c[t]*self.g[t]) for t in range(self.T)])
+        
+        # admm penalty
+        kappa = 5
+        obj2 = (kappa/2.0) * grb.quicksum([self.g[t] * self.g[t] for t in range(self.T)])
+        a = [gamma[t] + (kappa/2.0)*(p_util[t] + p_res[t])\
+             for t in range(self.T)]
+        obj3 = (kappa/2.0) * grb.quicksum([self.g[t] * a[t] for t in range(self.T)])
+        
+        # total objective
+        self.model.setObjective(obj1+obj2-obj3)
+        return
+    
+    def solve(self,grbpath):
+        # Write the LP problem
+        self.model.write(grbpath+"load-schedule-agent.lp")
+        
+        # Set up solver settings
+        grb.setParam('OutputFlag', 0)
+        grb.setParam('Heuristics', 0)
+        
+        # Open log file
+        logfile = open(grbpath+'gurobi-agent.log', 'w')
+        
+        # Pass data into my callback function
+        self.model._lastiter = -grb.GRB.INFINITY
+        self.model._lastnode = -grb.GRB.INFINITY
+        self.model._logfile = logfile
+        self.model._vars = self.model.getVars()
+        
+        # Solve model and capture solution information
+        self.model.optimize(mycallback)
+        
+        # Close log file
+        logfile.close()
+        if self.model.SolCount == 0:
+            print('No solution found, optimization status = %d' % self.model.Status)
+            sys.exit(0)
+        else:
+            fixed = ['base', 'hvac', 'hoth2o']
+            devices = [d for dtype in self.data if dtype != "ORG" \
+                        for d in self.data[dtype] if d not in fixed]
+            p_opt = {(d,t): self.p[(d,t)].getAttr("x") for d in devices \
+                      for t in range(self.T)}
+        
+            # Store optimal schedule in the attribute
+            self.p_sch = {d: [p_opt[(d,t)] for t in range(self.T)] for d in devices}
+            self.g_opt = [self.g[t].getAttr("x") for t in range(self.T)]
+            return
+        
 
-
+class Utility:
+    def __init__(self,graph,P_util,P_sch,Gamma):
+        self.nodes = [n for n in graph if graph.nodes[n]['label'] != 'S']
+        self.res = [n for n in graph if graph.nodes[n]['label'] == 'H']
+        self.N = len(self.nodes)
+        self.T = len(Gamma[self.res[0]])
+        
+        self.model = grb.Model(name="Get Optimal Utility Estimated Schedule")
+        self.model.ModelSense = grb.GRB.MINIMIZE
+        self.variables()
+        self.network(graph)
+        self.set_objective(P_util,P_sch,Gamma)
+        return
+    
+    def variables(self):
+        self.g = {(n,t):self.model.addVar(vtype=grb.GRB.BINARY,
+                                          name="g_{0}_{1}".format(n,t)) \
+                  for n in self.res for t in range(self.T)}
+        return
+    
+    def network(self,graph,vmin=0.95,vmax=1.05):
+        R = compute_Rmat(graph)
+        vlow = (vmin*vmin - 1)
+        vhigh = (vmax*vmax - 1)
+        
+        # Indices of residence nodes in the R matrix
+        resind = [i for i,n in enumerate(self.nodes) if n in self.res]
+        
+        for i in resind:
+            for t in range(self.T):
+                self.model.addConstr(
+                    -grb.quicksum([R[i,j]*self.g[(self.nodes[j],t)] \
+                                   for j in resind]) <= vhigh)
+                self.model.addConstr(
+                    -grb.quicksum([R[i,j]*self.g[(self.nodes[j],t)] \
+                                  for j in resind]) >= vlow)
+        return
+    
+    def set_objective(self,p_util,p_res,gamma):
+        # admm penalty
+        kappa = 5
+        obj1 = (kappa/2.0) * grb.quicksum([self.g[(n,t)] * self.g[(n,t)] \
+                                           for n in self.res for t in range(self.T)])
+        obj2 = 0
+        for n in self.res:
+            a = [gamma[n][t] - (kappa/2.0)*(p_util[n][t] + p_res[n][t])\
+                 for t in range(self.T)]
+            obj2 += (kappa/2.0) * grb.quicksum([self.g[(n,t)] * a[t] \
+                                                for t in range(self.T)])
+        
+        # total objective
+        self.model.setObjective(obj1+obj2)
+        return
+    
+    def solve(self,grbpath):
+        # Write the LP problem
+        self.model.write(grbpath+"load-schedule-utility.lp")
+        
+        # Set up solver settings
+        grb.setParam('OutputFlag', 0)
+        grb.setParam('Heuristics', 0)
+        
+        # Open log file
+        logfile = open(grbpath+'gurobi-utility.log', 'w')
+        
+        # Pass data into my callback function
+        self.model._lastiter = -grb.GRB.INFINITY
+        self.model._lastnode = -grb.GRB.INFINITY
+        self.model._logfile = logfile
+        self.model._vars = self.model.getVars()
+        
+        # Solve model and capture solution information
+        self.model.optimize(mycallback)
+        
+        # Close log file
+        logfile.close()
+        if self.model.SolCount == 0:
+            print('No solution found, optimization status = %d' % self.model.Status)
+            sys.exit(0)
+        else:
+            self.g_opt = {h: [self.g[(h,t)].getAttr("x") \
+                              for t in range(self.T)] for h in self.res}
+            return
 
 
 
--- opt-usage-toy5.py
+++ opt-usage-toy5.py
@@ -11,6 +11,7 @@ import networkx as nx
 import pandas as pd
 from math import ceil
 from shapely.geometry import LineString
+import matplotlib.pyplot as plt
 
 workpath = os.getcwd()
 rootpath = os.path.dirname(workpath)
@@ -26,8 +27,6 @@ homepath = inppath + "VA121_20140723.csv"
 sys.path.append(libpath)
 # from pySchedLoadlib import Central
 print("Imported modules")
-
-
 
 #%% Home definition
 
@@ -85,7 +84,6 @@ homeid = [511210207001189,51121020900342,511210203001855,511210207001113,
 COST = [1]*24
 
 
-
 #%% Network definition
 dist = nx.Graph()
 edgelist = [(0,10),(10,11),(11,12),(12,13),(13,14),(11,1),(1,2),(12,3),(12,4),
@@ -98,12 +96,13 @@ ncord = {0:[0,0],1:[1,-1],2:[1,-2],3:[2,-1],4:[2,1],5:[4,-1],6:[4,-2],
 elabel = {(0,10):'E',(10,11):'P',(11,12):'P',(12,13):'P',(13,14):'P',
           (11,1):'S',(1,2):'S',(12,3):'S',(12,4):'S',(14,5):'S',(5,6):'S'}
 
-util = 2
+util = 1
 prefix = "central-control"+"net0"+"pos0"
 # prefix = "alllowload"+"net0"+"pos0"
 
-e_r = {(0,10):1e-12, (10,11):0.001*util,(11,12):0.001,(12,13):0.001,(13,14):0.001,
+e_r = {(0,10):1e-12, (10,11):0.001,(11,12):0.001,(12,13):0.001,(13,14):0.001*util,
           (11,1):0.0005,(1,2):0.0005,(12,3):0.0005,(12,4):0.0005,(14,5):0.0005,(5,6):0.0005}
+
 
 
 nx.set_edge_attributes(dist, e_r, 'r')
@@ -115,43 +114,89 @@ nx.set_node_attributes(dist, nlabel, 'label')
 for e in edgelist:
     dist.edges[e]['geometry'] = LineString((ncord[e[0]],ncord[e[1]]))
 
-
-
 # Home data
 homeid = [51121021300494]*4+[511210207001189]*2
 # homeid = [511210207001189]*6
 # homeid = [51121021300494]*6
 homes = {k+1:all_homes[h] for k,h in enumerate(homeid)}
 
-#%% Central algorithm
-from pySchedLoadlib import Central
-c = Central(homes,dist,COST)
-c.solve(grbpath)
-
-#Plot incentive evolution
-import matplotlib.pyplot as plt
+#%% Distributed algorithm
+from pySchedLoadlib import Home,Utility
 
 homelist = sorted([n for n in dist if dist.nodes[n]['label'] == 'H'])
+
+
+
+P_est = {0:{h:[0]*len(COST) for h in homelist}}
+P_sch = {0:{h:[0]*len(COST) for h in homelist}}
+G = {0:{h:[0]*len(COST) for h in homelist}}
+S = {}
+
+iter_max = 50
+error = [1]
+
+k = 0
+eps = 1
+while(k <= iter_max) and (eps>1e-2):
+    # solve utility level problem to get estimate
+    U_obj = Utility(dist,P_est[k],P_sch[k],G[k])
+    U_obj.solve(grbpath)
+    P_est[k+1] = U_obj.g_opt
+    
+    
+    # solve individual agent level problem
+    P_sch[k+1] = {}
+    S[k+1] = {}
+    for h in homelist:
+        H_obj = Home(COST,homes[h],P_est[k][h],P_sch[k][h],G[k][h])
+        H_obj.solve(grbpath)
+        P_sch[k+1][h] = H_obj.g_opt
+        S[k+1][h] = H_obj.p_sch
+    
+    
+    # update dual variables
+    kappa = 5
+    G[k+1] = {}
+    diff = []
+    for h in homelist:
+        check = [(P_est[k+1][h][t] - P_sch[k+1][h][t]) for t in range(len(COST))]
+        G[k+1][h] = [G[k][h][t] + (kappa/2) * check[t]for t in range(len(COST))]
+        diff.append(np.linalg.norm(np.array(check))/len(COST))
+    eps = np.linalg.norm(np.array(diff))/len(diff)
+    error.append(eps)
+    
+    k = k + 1
+
+
+
+#%% Plot incentive evolution
 xarray = np.linspace(0,25,25)
-
-
+iterations = k
+l = iterations-5
 
 for device in ['laundry','dwasher']:
     fig = plt.figure(figsize = (20,16))
     for i,h in enumerate(homelist):
         ax = fig.add_subplot(6,1,i+1)
         if device in homes[h]["SL"]:
-            ax.step(xarray,[0]+c.p_sch[h][device])
+            # ax.step(xarray,[0]+homes[h]["ORG"][device],label="original profile")
+            for m in range(l,iterations+1):
+                ax.step(xarray,[0]+S[m][h][device],
+                        label="iter="+str(m))
+            ax.legend(ncol=5,prop={'size': 15})
         ax.set_ylabel("Sch. Load (kW)",fontsize=15)
     fig.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-'+device),
                 bbox_inches='tight')
 
 
+
 fig2 = plt.figure(figsize=(20,16))
 for i,h in enumerate(homelist):
     ax2 = fig2.add_subplot(6,1,i+1)
-    ax2.step(xarray,[0]+c.g_opt[h])
-        
+    for m in range(l,iterations+1):
+        ax2.step(xarray,[0]+P_sch[m][h],
+                 label="iter="+str(m))
+        ax2.legend(ncol=5,prop={'size': 15})
     ax2.set_ylabel("Total load (kW)",fontsize=15)
 fig2.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-total'),
              bbox_inches='tight')
@@ -160,12 +205,18 @@ fig2.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-total'),
 fig3 = plt.figure(figsize=(20,16))
 for i,h in enumerate(homelist):
     ax3 = fig3.add_subplot(6,1,i+1)
-    ax3.step(xarray,np.insert(c.a_opt[h],0,0))
+    for m in range(l,iterations+1):
+        ax3.step(xarray,[0]+G[m][h],
+                 label="iter="+str(m))
+        ax3.legend(ncol=5,prop={'size': 15})
     ax3.set_ylabel("Incentive signal",fontsize=15)
 fig3.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-incentive'),
              bbox_inches='tight')
 
-
-
-
+fig4 = plt.figure(figsize=(20,16))
+ax4 = fig4.add_subplot(1,1,1)
+ax4.plot(range(iterations),error[1:])
+ax4.set_ylabel("Difference (pu)",fontsize=15)
+fig4.savefig("{}{}.png".format(figpath,prefix+'-toy-usage-convergence'),
+              bbox_inches='tight')
 
