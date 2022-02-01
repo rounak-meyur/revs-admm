02e021bcf07f961d84d9b77f9738be801fdf4d47
Rounak Meyur <45717264+rounak-meyur@users.noreply.github.com>
2022-02-01 17:48:00 -0500

    updated output

--- opt-ev-charge-toy3.py
+++ opt-ev-charge-toy3.py
@@ -51,14 +51,13 @@ sepind = [i+1 for i,l in enumerate(lines) if l.strip("\n").endswith("##")]
 
 res_lines = lines[sepind[1]:sepind[2]-1]
 ev_lines = lines[sepind[3]:sepind[4]-1]
-soc_lines = lines[sepind[5]:len(lines)]
-# soc_lines = lines[sepind[5]+1:sepind[6]]
-# diff_lines = lines[sepind[7]+1:len(lines)]
+soc_lines = lines[sepind[5]:sepind[6]-1]
+diff_lines = lines[sepind[7]:len(lines)]
 
 P_res = get_data(res_lines)
 P_ev = get_data(ev_lines)
 SOC = get_data(soc_lines)
-
+diff = get_data(diff_lines)
 
 
 
@@ -78,22 +77,6 @@ res = [n for n in dist if dist.nodes[n]['label']=='H']
 # res_interest = [n for n in res if (xmin<=dist.nodes[n]['cord'][0]<=xmax) and (ymin<=dist.nodes[n]['cord'][1]<=ymax)]
 
 
-#%% Plot the convergence
-# num_iter = len(diff)
-# xtix = range(1,num_iter+1)
-# fig4 = plt.figure(figsize=(20,16))
-# ax4 = fig4.add_subplot(1,1,1)
-# for h in ev_home:
-#     ax4.plot(xtix,[diff[k][h] for k in xtix],
-#               label="Home "+str(h))
-# ax4.set_ylabel("Difference",fontsize=25)
-# ax4.set_xlabel("Iterations",fontsize=25)
-# ax4.legend(loc='best',ncol=1,prop={'size': 25})
-# ax4.set_xticks(list(range(0,21,5)))
-# ax4.tick_params(axis='y', labelsize=25)
-# ax4.tick_params(axis='x', labelsize=25)
-# fig4.savefig("{}{}.png".format(figpath,prefix+'-convergence'),
-#               bbox_inches='tight')
 
 
 #%% Voltage impact
@@ -103,9 +86,10 @@ nodelist = [n for n in dist if dist.nodes[n]['label']!='S']
 
 
 #%% Multiple voltage profiles
-a = 30
-fig5 = plt.figure(figsize=(20,16))
-for l,r in enumerate([800, 900, 1000, 1100, 1200, 1300, 1400, 1500]):
+a = 10
+fig4 = plt.figure(figsize=(60,48))
+fig5 = plt.figure(figsize=(60,48))
+for l,r in enumerate([800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600]):
     # Extract data
     prefix = "agentEV-"+str(a)+"-adopt"+str(int(r))+"Watts.txt"
     with open(outpath+prefix,'r') as f:
@@ -113,7 +97,9 @@ for l,r in enumerate([800, 900, 1000, 1100, 1200, 1300, 1400, 1500]):
     
     sepind = [i+1 for i,l in enumerate(lines) if l.strip("\n").endswith("##")]
     res_lines = lines[sepind[1]:sepind[2]-1]
+    diff_lines = lines[sepind[7]:len(lines)]
     P_res = get_data(res_lines)
+    diff = get_data(diff_lines)
     
     # Compute voltages at nodes
     P = np.zeros(shape=(len(nodelist),T))
@@ -129,6 +115,21 @@ for l,r in enumerate([800, 900, 1000, 1100, 1200, 1300, 1400, 1500]):
     # Add the plot to figure
     ax5 = fig5.add_subplot(3,3,l+1)
     d = ax5.boxplot(volt_data)
+    
+    # Plot the convergence
+    num_iter = 15
+    xtix = range(1,num_iter+1)
+    ax4 = fig4.add_subplot(3,3,l+1)
+    for h in ev_home:
+        ax4.plot(xtix,[diff[h][k] for k in range(num_iter)],
+                  label="Home "+str(h))
+    ax4.set_ylabel("Difference")
+    ax4.set_xlabel("Iterations")
+    ax4.legend(loc='best',ncol=1,prop={'size': 5})
+    ax4.set_xticks(list(range(0,num_iter+1,5)))
+    ax4.tick_params(axis='y')
+    ax4.tick_params(axis='x')
+    
     
 
 
