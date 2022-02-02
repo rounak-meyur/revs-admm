4a8d7b7c81af71b187d76737a752c446a56a7647
Rounak Meyur <45717264+rounak-meyur@users.noreply.github.com>
2022-02-02 07:45:15 -0500

    updated test code

--- opt-ev-charge-toy3.py
+++ opt-ev-charge-toy3.py
@@ -70,11 +70,13 @@ dist = GetDistNet(distpath,sub)
 res = [n for n in dist if dist.nodes[n]['label']=='H']
 
 # Area of interest where EV adoption is studied
-# xmin = -80.37217
-# ymin = 37.1993
-# xmax = xmin + 0.010
-# ymax = ymin + 0.005
-# res_interest = [n for n in res if (xmin<=dist.nodes[n]['cord'][0]<=xmax) and (ymin<=dist.nodes[n]['cord'][1]<=ymax)]
+xmin = -80.37217
+ymin = 37.1993
+xmax = xmin + 0.010
+ymax = ymin + 0.005
+res_interest = [n for n in res \
+                if (xmin<=dist.nodes[n]['cord'][0]<=xmax) \
+                    and (ymin<=dist.nodes[n]['cord'][1]<=ymax)]
 
 
 
@@ -86,49 +88,67 @@ nodelist = [n for n in dist if dist.nodes[n]['label']!='S']
 
 
 #%% Multiple voltage profiles
-a = 10
-fig4 = plt.figure(figsize=(60,48))
-fig5 = plt.figure(figsize=(60,48))
-for l,r in enumerate([800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600]):
-    # Extract data
-    prefix = "agentEV-"+str(a)+"-adopt"+str(int(r))+"Watts.txt"
-    with open(outpath+prefix,'r') as f:
-        lines = f.readlines()
-    
-    sepind = [i+1 for i,l in enumerate(lines) if l.strip("\n").endswith("##")]
-    res_lines = lines[sepind[1]:sepind[2]-1]
-    diff_lines = lines[sepind[7]:len(lines)]
-    P_res = get_data(res_lines)
-    diff = get_data(diff_lines)
-    
-    # Compute voltages at nodes
-    P = np.zeros(shape=(len(nodelist),T))
-    Z = np.ones(shape=(len(nodelist),T)) * vset
-    for i,n in enumerate(nodelist):
-        if n in res:
-            P[i,:] = np.array(P_res[n])
-    
-    V = np.sqrt(Z - R@P)
-    volt = {h:V[i,:].tolist() for i,h in enumerate(nodelist) if h in nodelist}
-    volt_data = [[volt[n][t] for n in res] for t in range(T)]
-    
-    # Add the plot to figure
-    ax5 = fig5.add_subplot(3,3,l+1)
-    d = ax5.boxplot(volt_data)
-    
-    # Plot the convergence
-    num_iter = 15
-    xtix = range(1,num_iter+1)
-    ax4 = fig4.add_subplot(3,3,l+1)
-    for h in ev_home:
-        ax4.plot(xtix,[diff[h][k] for k in range(num_iter)],
-                  label="Home "+str(h))
-    ax4.set_ylabel("Difference")
-    ax4.set_xlabel("Iterations")
-    ax4.legend(loc='best',ncol=1,prop={'size': 5})
-    ax4.set_xticks(list(range(0,num_iter+1,5)))
-    ax4.tick_params(axis='y')
-    ax4.tick_params(axis='x')
+import matplotlib.patches as patches
+
+
+colors = ['pink','lightblue','lightgreen','lightsalmon','lightyellow',
+          'orchid','bisque','aquamarine','slateblue']
+labels = [(c+s)%24 for c in range(11,24)]
+
+
+for a in [10,20,30,40,50,60]:
+    # fig4 = plt.figure(figsize=(60,48))
+    fig = plt.figure(figsize=(40,32))
+    ax = fig.add_subplot(1,1,1)
+    leghands = []
+    for b,r in enumerate([800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600]):
+        # Extract data
+        prefix = "agentEV-"+str(a)+"-adopt"+str(int(r))+"Watts.txt"
+        with open(outpath+prefix,'r') as f:
+            lines = f.readlines()
+        
+        sepind = [i+1 for i,l in enumerate(lines) if l.strip("\n").endswith("##")]
+        res_lines = lines[sepind[1]:sepind[2]-1]
+        diff_lines = lines[sepind[7]:len(lines)]
+        P_res = get_data(res_lines)
+        diff = get_data(diff_lines)
+        
+        # Compute voltages at nodes
+        P = np.zeros(shape=(len(nodelist),T))
+        Z = np.ones(shape=(len(nodelist),T)) * vset
+        for i,n in enumerate(nodelist):
+            if n in res:
+                P[i,:] = np.array(P_res[n])
+        
+        V = np.sqrt(Z - R@P)
+        volt = {h:V[i,:].tolist() for i,h in enumerate(nodelist) if h in nodelist}
+        volt_data = [[volt[n][t] for n in res_interest] for t in range(11,24)]
+        
+        # Add the plot to figure
+        bplot = ax.boxplot(volt_data,patch_artist=True,labels=labels)
+        ax.tick_params(axis='y',labelsize=50)
+        ax.tick_params(axis='x',labelsize=50)
+        ax.set_ylabel("Node Voltage",fontsize=50)
+        ax.set_xlabel("Hours",fontsize=50)
+        for t in range(len(labels)):
+            bplot['boxes'][t].set_facecolor(colors[b])
+        
+        leghands.append(patches.Rectangle((0,0),1,1, facecolor=colors[b],
+                               label=str(r)+" Watts"))
+        ax.legend(handles=leghands,loc='best',ncol=3,prop={'size': 40})
+        # # Plot the convergence
+        # num_iter = 15
+        # xtix = range(1,num_iter+1)
+        # ax4 = fig4.add_subplot(3,3,l+1)
+        # for h in ev_home:
+        #     ax4.plot(xtix,[diff[h][k] for k in range(num_iter)],
+        #               label="Home "+str(h))
+        # ax4.set_ylabel("Difference",fontsize=25)
+        # ax4.set_xlabel("Iterations",fontsize=25)
+        # ax4.legend(loc='best',ncol=2,prop={'size': 25})
+        # ax4.set_xticks(list(range(0,num_iter+1,5)))
+        # ax4.tick_params(axis='y',labelsize=25)
+        # ax4.tick_params(axis='x',labelsize=25)
     
     
 
